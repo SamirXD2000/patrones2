92d0dda55a10ec84d234afe5f1756a474c0de9d6
SamirXD2000 <47357184+SamirXD2000@users.noreply.github.com>
2019-02-24 10:53:29 -0600

    T3

---
+++ Tarea3/BDTAREA.sql
@@ -0,0 +1,6 @@
+create database tarea3;
+use tarea3;
+
+insert into "Usuario"(nombre, apellido, apodo, id) values('Samir', 'Jimenez', 'Samo', '1234');
+insert into "Usuario"(nombre, apellido, apodo, id) values('David', 'Segovia', 'Segobaby', '1235');
+insert into "Usuario"(nombre, apellido, apodo, id) values('Jesus', 'Silva', 'Yisus', '1236');
---
+++ Tarea3/Tarea3/Model/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Tarea3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            String cs = @"Data Source=DESKTOP-KSNKPUR\SQLEXPRESS;Initial Catalog=tarea3;Integrated Security=True";
+            String sql = @"SELECT * FROM USUARIO";
+            DataTable tabla = new DataTable();
+            SqlConnection c = new SqlConnection(cs);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = new SqlCommand(sql, c);
+            adapter.Fill(tabla);
+            DataGrid1.DataContext = tabla;
+            c.Close();
+
+        }
+    }
+}
